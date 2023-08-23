38831e74e7781280c3b4df8f8954cd7cb89b9c8a
Jake Drysdale <drysdalejake95@gmail.com>
2023-08-23 16:17:46 +0100

    init commit

---
+++ typing-animation.js
@@ -0,0 +1,47 @@
+// Get the title element
+const titleElement = document.getElementById('typing-title');
+
+
+// Define the text you want to type in the animation
+const typingText = [
+    "Welcome to my portfolio.",
+    "I am a passionate researcher.",
+    "I enjoy creating music.",
+    // Add more lines of text as needed
+];
+
+// Time interval for typing animation (in milliseconds)
+const typingInterval = 100; // Adjust as needed
+
+// Start the typing animation
+let textIndex = 0;
+let charIndex = 0;
+
+function typeText() {
+    if (textIndex < typingText.length) {
+        if (charIndex < typingText[textIndex].length) {
+            titleElement.textContent += typingText[textIndex].charAt(charIndex);
+            charIndex++;
+            setTimeout(typeText, typingInterval);
+        } else {
+            setTimeout(eraseText, 1000); // Pause after typing a line
+        }
+    } else {
+        textIndex = 0; // Restart the animation loop
+        setTimeout(typeText, typingInterval);
+    }
+}
+
+function eraseText() {
+    if (charIndex > 0) {
+        titleElement.textContent = typingText[textIndex].substring(0, charIndex - 1);
+        charIndex--;
+        setTimeout(eraseText, typingInterval / 2);
+    } else {
+        textIndex++;
+        setTimeout(typeText, typingInterval);
+    }
+}
+
+// Start the typing animation on page load
+typeText();
